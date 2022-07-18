e1de4151b6568246a3d8ad0ecbe1b6fba470fce7
ck <2658868303@qq.com>
2022-07-18 15:12:58 +0800

    7月18日提交的内容,复习一下提交代码的步骤,上传到远程库git-demo

---
+++ src/main/java/com/zk/demo/demo_01.java
@@ -0,0 +1,9 @@
+package com.zk.demo;
+
+public class demo_01 {
+    public static void main(String[] args) {
+        System.out.println("demo01");
+        System.out.println("上传到远程库");
+        System.out.println("上传到远程库");
+    }
+}
