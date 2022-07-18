9b47ee23810456b863eaa38550321ba56746c7bf
chenchenfan <100469349+chenchenfan@users.noreply.github.com>
2022-07-18 16:21:00 +0800

    Update demo_02.java
    把demo1 修改成了 demo2

--- src/main/java/com/zk/demo/demo_02.java
+++ src/main/java/com/zk/demo/demo_02.java
@@ -2,7 +2,7 @@ package com.zk.demo;
 
 public class demo_02 {
     public static void main(String[] args) {
-        System.out.println("demo01");
+        System.out.println("demo02");
         System.out.println("上传到远程库");
         System.out.println("上传到远程库");
         System.out.println("已上传到远程库");
