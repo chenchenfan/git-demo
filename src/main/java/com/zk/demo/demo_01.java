eea3d650e3bddad51f338c01eb7d24813a4bcd41
chenchenfan <100469349+chenchenfan@users.noreply.github.com>
2022-07-18 16:32:14 +0800

    Update demo_01.java
    把 demo011 修改成了0111111111111111111

--- src/main/java/com/zk/demo/demo_01.java
+++ src/main/java/com/zk/demo/demo_01.java
@@ -2,7 +2,7 @@ package com.zk.demo;
 
 public class demo_01 {
     public static void main(String[] args) {
-        System.out.println("demo011");
+        System.out.println("demo01111111111111111");
         System.out.println("上传到远程库");
         System.out.println("上传到远程库");
         System.out.println("已上传到远程库");
