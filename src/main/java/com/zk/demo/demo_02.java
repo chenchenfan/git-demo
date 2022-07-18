4225b9e131e15d2cca63141fd5d384311699e238
ck <2658868303@qq.com>
2022-07-18 16:19:49 +0800

    7月18日,创建的第二个测试类

---
+++ src/main/java/com/zk/demo/demo_02.java
@@ -0,0 +1,15 @@
+package com.zk.demo;
+
+public class demo_02 {
+    public static void main(String[] args) {
+        System.out.println("demo01");
+        System.out.println("上传到远程库");
+        System.out.println("上传到远程库");
+        System.out.println("已上传到远程库");
+        System.out.println("已上传到远程库");
+        System.out.println("远程库修改的");
+        System.out.println("远程库修改的");
+        System.out.println("远程库修改的");
+        System.out.println("远程库修改的");
+    }
+}
